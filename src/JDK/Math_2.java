9acc2725a154ce952876a7c88a10b69b9bf3e04b
lovejj1994 <455234037@qq.com>
2017-03-09 14:39:20 +0800

    2017.3.9   jdk8 比较类，字符串join，Math类等

---
+++ src/JDK/Math_2.java
@@ -0,0 +1,17 @@
+package JDK;
+
+import java.util.Arrays;
+
+/**
+ * Created by panqian on 2017/2/17.
+ */
+public class Math_2 {
+
+    public static void main(String[] args) {
+
+        //余数应该不是负数
+        System.out.println(-3%2);
+        //做调整
+        System.out.println(Math.floorMod(-3,2));
+    }
+}
---
+++ src/JDK/String/Join_1.java
@@ -0,0 +1,12 @@
+package JDK.String;
+
+/**
+ * String 分隔符
+ * Created by panqian on 2017/3/9.
+ */
+public class Join_1 {
+    public static void main(String[] args) {
+        String join = String.join(",", "my", "name", "is");
+        System.out.println(join);
+    }
+}
